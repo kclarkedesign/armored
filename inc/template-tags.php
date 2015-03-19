d7f1a45af2ed824897eabd6cb4a4e5b8e24e57f7
Kirk Clarke <kclarke.design@gmail.com>
2015-03-18 21:51:30 -0400

    created additonal templates, single post, post attachements and 404

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -65,3 +65,55 @@ function armored_category_transient_flusher() {
 }
 add_action( 'edit_category', 'armored_category_transient_flusher' );
 add_action( 'save_post', 'armored_category_transient_flusher' );
+
+if ( ! functioon_exists( 'armored_content_nav' ) ):
+/**
+ * Display navigation to next/previous pages when applicable
+ * 
+ * @since Armored 1.0
+ */
+function armored_content_nav( $nav_id ) {
+    global $wp_query, $post;
+ 
+    // Don't print empty markup on single pages if there's nowhere to navigate.
+    if ( is_single() ) {
+        $previous = ( is_attachment() ) ? get_post( $post->post_parent ) : get_adjacent_post( false, '', true );
+        $next = get_adjacent_post( false, '', false );
+ 
+        if ( ! $next && ! $previous )
+            return;
+    }
+ 
+    // Don't print empty markup in archives if there's only one page.
+    if ( $wp_query->max_num_pages < 2 && ( is_home() || is_archive() || is_search() ) )
+        return;
+ 
+    $nav_class = 'site-navigation paging-navigation';
+    if ( is_single() )
+        $nav_class = 'site-navigation post-navigation';
+ 
+    ?>
+    <nav role="navigation" id="<?php echo $nav_id; ?>" class="<?php echo $nav_class; ?>">
+        <h1 class="assistive-text"><?php _e( 'Post navigation', 'armored' ); ?></h1>
+ 
+    <?php if ( is_single() ) : // navigation links for single posts ?>
+ 
+        <?php previous_post_link( '<div class="nav-previous">%link</div>', '<span class="meta-nav">' . _x( '&larr;', 'Previous post link', 'armored' ) . '</span> %title' ); ?>
+        <?php next_post_link( '<div class="nav-next">%link</div>', '%title <span class="meta-nav">' . _x( '&rarr;', 'Next post link', 'armored' ) . '</span>' ); ?>
+ 
+    <?php elseif ( $wp_query->max_num_pages > 1 && ( is_home() || is_archive() || is_search() ) ) : // navigation links for home, archive, and search pages ?>
+ 
+        <?php if ( get_next_posts_link() ) : ?>
+        <div class="nav-previous"><?php next_posts_link( __( '<span class="meta-nav">&larr;</span> Older posts', 'armored' ) ); ?></div>
+        <?php endif; ?>
+ 
+        <?php if ( get_previous_posts_link() ) : ?>
+        <div class="nav-next"><?php previous_posts_link( __( 'Newer posts <span class="meta-nav">&rarr;</span>', 'armored' ) ); ?></div>
+        <?php endif; ?>
+ 
+    <?php endif; ?>
+ 
+    </nav><!-- #<?php echo $nav_id; ?> -->
+    <?php
+}
+endif; // armored_content_nav
--- index.php
+++ index.php
@@ -16,17 +16,32 @@
 
 
 <div id="primary" class="content-area">
- 	<div id="content" class="site-content">
-     	<?php /* Start the Loop */ ?>
-    	<?php while ( have_post() ) : the_post(), ?>
-     		<div class="entry-summary">
-     			<?php the_excerpt(); ?>
-			</div><!-- .entry-summary -->
-     	<?php endwhile; ?>
-	</div>
-    <!-- #content .site-content -->
-</div>
-<!-- #primary .content-area -->
+    <div id="content" class="site-content" role="main">
+ 
+    <?php if ( have_posts() ) : ?>
+ 
+        <?php armored_content_nav( 'nav-above' ); ?>
+ 
+        <?php /* Start the Loop */ ?>
+        <?php while ( have_posts() ) : the_post(); ?>
+ 
+            <?php
+                /* Include the Post-Format-specific template for the content.
+                 * If you want to overload this in a child theme then include a file
+                 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+                 */
+                get_template_part( 'content', get_post_format() );
+            ?>
+ 
+        <?php endwhile; ?>
+ 
+        <?php armored_content_nav( 'nav-below' ); ?>
+    <?php else : ?>
+        <?php get_template_part( 'no-results', 'index' ); ?>
+    <?php endif; ?>
+ 
+    </div><!-- #content .site-content -->
+</div><!-- #primary .content-area -->
 
 
 <?php get_siebar(); ?>
