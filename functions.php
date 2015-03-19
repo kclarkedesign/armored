489b3174976e971113e36260433157cd9445955a
Kirk Clarke <kclarke.design@gmail.com>
2015-03-19 00:58:58 -0400

    fixed basic errors, still need to solve page error

--- functions.php
+++ functions.php
@@ -1,7 +1,7 @@
 <?php
 /**
- * Armored functions and definitions
- * 
+ * armored functions and definitions
+ *
  * @package Armored
  * @since Armored 1.0
  */
@@ -9,59 +9,57 @@
 /**
  * Set the content width based on the theme's design and stylesheet.
  *
- * @since Armored 1.0
+ * @since armored 1.0
  */
- if ( ! isset( $content_width ) ) {
- 	 $content_width = 654; /*pixels */
- }
+if ( ! isset( $content_width ) )
+  $content_width = 654; /* pixels */
 
- if ( ! function_exists( 'armored_setup' ) );
- /**
-  * Sets up theme defaults and registers support for various Wordpress features.
-  * 
-  * Note that this function is hooked into the after_setup_theme hook, which runs
-  * before the init hook. The init hook is too late for some features, such as indicating
-  * support post thumbnails.
-  * 
-  * @since Armored 1.0
-  */
-  function armored_setup() {
+if ( ! function_exists( 'armored_setup' ) ):
+/**
+ * Sets up theme defaults and registers support for various WordPress features.
+ *
+ * Note that this function is hooked into the after_setup_theme hook, which runs
+ * before the init hook. The init hook is too late for some features, such as indicating
+ * support post thumbnails.
+ *
+ * @since armored 1.0
+ */
+function armored_setup() {
 
-  	 /**
-  	  * Custom template tage for this theme.
-  	  */
-  	 require( get_template_directory() . '/inc/template-tags.php' );
+  /**
+   * Custom template tags for this theme.
+   */
+  require( get_template_directory() . '/inc/template-tags.php' );
 
-  	 /**
-  	 * Custom functions that act independently of the theme templates
-  	 */
-  	 require( get_template_directory() . '/inc/tweaks.php' );
+  /**
+   * Custom functions that act independently of the theme templates
+   */
+  require( get_template_directory() . '/inc/tweaks.php' );
 
-  	 /**
-  	 * Make theme available for translation
-  	 * Translations can be filled in the /languages/ directory
-  	 * If you're building a theme based on Armored, use a find a replace
-  	 * to change 'armored' to the name of your theme in all template files
-  	 */
-  	load_theme_textdomain( 'armored' , get_template_directory() . '/languages' );
+  /**
+   * Make theme available for translation
+   * Translations can be filed in the /languages/ directory
+   * If you're building a theme based on armored, use a find and replace
+   * to change 'armored' to the name of your theme in all the template files
+   */
+  load_theme_textdomain( 'armored', get_template_directory() . '/languages' );
 
-  	/**
-  	 * Add default posts and comments RSS feed links to head
-  	 */
-  	add_theme_support( 'automatic-feed-links' );
+  /**
+   * Add default posts and comments RSS feed links to head
+   */
+  add_theme_support( 'automatic-feed-links' );
 
-  	/**
-  	 * Enable support for the Aside Post Format
-  	 */
-  	add_theme_support( 'post-formats', 'armored' ) );
+  /**
+   * Enable support for the Aside Post Format
+   */
+  add_theme_support( 'post-formats', array( 'aside' ) );
 
-	/**
-	 * This theme uses wp_nav_menu() in one locations.
-	 */
-	register_nav_menus( array(
-		'primary' => __( 'Primary Menu', 'armored' ),
-		//'social'  => __( 'Social Links Menu', 'armored' ),
-	) );
+  /**
+   * This theme uses wp_nav_menu() in one location.
+   */
+  register_nav_menus( array(
+    'primary' => __( 'Primary Menu', 'armored' ),
+  ) );
 
 }
 endif; // armored_setup
@@ -73,22 +71,22 @@ add_action( 'after_setup_theme', 'armored_setup' );
  * @since Armored 1.0
  **/
 function armored_widgets_init() {
-    register_sidebar( array(
-        'name' => __( 'Primary Widget Area', 'armored' ),
-        'id' => 'sidebar-1',
-        'before_widget' => '<aside id="%1$s" class="widget %2$s">',
-        'after_widget' => '</aside>',
-        'before_title' => '<h1 class="widget-title">',
-        'after_title' => '</h1>',
+  register_sidebar( array(
+    'name' => __( 'Primary Widget Area', 'armored' ),
+    'id' => 'sidebar-1',
+    'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+    'after_widget' => '</aside>',
+    'before_title' => '<h1 class="widget-title">',
+    'after_title' => '</h1>',
     ) );
- 
-    register_sidebar( array(
-        'name' => __( 'Secondary Widget Area', 'armored' ),
-        'id' => 'sidebar-2',
-        'before_widget' => '<aside id="%1$s" class="widget %2$s">',
-        'after_widget' => '</aside>',
-        'before_title' => '<h1 class="widget-title">',
-        'after_title' => '</h1>',
+
+  register_sidebar( array(
+    'name' => __( 'Secondary Widget Area', 'armored' ),
+    'id' => 'sidebar-2',
+    'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+    'after_widget' => '</aside>',
+    'before_title' => '<h1 class="widget-title">',
+    'after_title' => '</h1>',
     ) );
 }
 add_action( 'widgets_init', 'armored_widgets_init' );
@@ -100,13 +98,13 @@ function armored_scripts() {
   wp_enqueue_style( 'style' , get_stylesheet() );
 
   if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
-      wp_enqueue_scripts( 'comment_reply' );
+    wp_enqueue_scripts( 'comment_reply' );
   }
 
   wp_enqueue_script( 'navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20120206', true );
- 
+
   if ( is_singular() && wp_attachment_is_image() ) {
-        wp_enqueue_script( 'keyboard-image-navigation', get_template_directory_uri() . '/js/keyboard-image-navigation.js', array( 'jquery' ), '20120202' );
-    }
+    wp_enqueue_script( 'keyboard-image-navigation', get_template_directory_uri() . '/js/keyboard-image-navigation.js', array( 'jquery' ), '20120202' );
+  }
 }
 add_action( 'wp_enqueue_scripts', 'armored_scripts' );
--- inc/template-tags.php
+++ inc/template-tags.php
@@ -8,69 +8,11 @@
  * @since Armored 1.0 
  */
 
-if ( ! function_exists( 'armored_posted_on' ) ) :
-/**
- * Prints HTML with meta information for the current post-date/time and author.
- *
- * @since Armored 1.0
- */
-function armored_posted_on() {
-    printf( __( 'Posted on <a href="%1$s" title="%2$s" rel="bookmark"><time class="entry-date" datetime="%3$s" pubdate>%4$s</time></a><span class="byline"> by <span class="author vcard"><a class="url fn n" href="%5$s" title="%6$s" rel="author">%7$s</a></span></span>', 'armored' ),
-        esc_url( get_permalink() ),
-        esc_attr( get_the_time() ),
-        esc_attr( get_the_date( 'c' ) ),
-        esc_html( get_the_date() ),
-        esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ),
-        esc_attr( sprintf( __( 'View all posts by %s', 'armored' ), get_the_author() ) ),
-        esc_html( get_the_author() )
-    );
-}
-endif;
- 
-/**
- * Returns true if a blog has more than 1 category
- *
- * @since Armored 1.0
- */
-function armored_categorized_blog() {
-    if ( false === ( $all_the_cool_cats = get_transient( 'all_the_cool_cats' ) ) ) {
-        // Create an array of all the categories that are attached to posts
-        $all_the_cool_cats = get_categories( array(
-            'hide_empty' => 1,
-        ) );
- 
-        // Count the number of categories that are attached to the posts
-        $all_the_cool_cats = count( $all_the_cool_cats );
- 
-        set_transient( 'all_the_cool_cats', $all_the_cool_cats );
-    }
- 
-    if ( '1' != $all_the_cool_cats ) {
-        // This blog has more than 1 category so armored_categorized_blog should return true
-        return true;
-    } else {
-        // This blog has only 1 category so armored_categorized_blog should return false
-        return false;
-    }
-}
- 
-/**
- * Flush out the transients used in armored_categorized_blog
- *
- * @since Armored 1.0
- */
-function armored_category_transient_flusher() {
-    // Like, beat it. Dig?
-    delete_transient( 'all_the_cool_cats' );
-}
-add_action( 'edit_category', 'armored_category_transient_flusher' );
-add_action( 'save_post', 'armored_category_transient_flusher' );
-
-if ( ! functioon_exists( 'armored_content_nav' ) ):
+if ( ! function_exists( 'armored_content_nav' ) ):
 /**
  * Display navigation to next/previous pages when applicable
- * 
- * @since Armored 1.0
+ *
+ * @since armored 1.0
  */
 function armored_content_nav( $nav_id ) {
     global $wp_query, $post;
@@ -118,13 +60,71 @@ function armored_content_nav( $nav_id ) {
 }
 endif; // armored_content_nav
 
-if ( ! functinon_exists( 'armored_comment') );
+if ( ! function_exists( 'armored_posted_on' ) ) :
+/**
+ * Prints HTML with meta information for the current post-date/time and author.
+ *
+ * @since armored 1.0
+ */
+function armored_posted_on() {
+    printf( __( 'Posted on <a href="%1$s" title="%2$s" rel="bookmark"><time class="entry-date" datetime="%3$s" pubdate>%4$s</time></a><span class="byline"> by <span class="author vcard"><a class="url fn n" href="%5$s" title="%6$s" rel="author">%7$s</a></span></span>', 'armored' ),
+        esc_url( get_permalink() ),
+        esc_attr( get_the_time() ),
+        esc_attr( get_the_date( 'c' ) ),
+        esc_html( get_the_date() ),
+        esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ),
+        esc_attr( sprintf( __( 'View all posts by %s', 'armored' ), get_the_author() ) ),
+        esc_html( get_the_author() )
+    );
+}
+endif;
+ 
+/**
+ * Returns true if a blog has more than 1 category
+ *
+ * @since armored 1.0
+ */
+function armored_categorized_blog() {
+    if ( false === ( $all_the_cool_cats = get_transient( 'all_the_cool_cats' ) ) ) {
+        // Create an array of all the categories that are attached to posts
+        $all_the_cool_cats = get_categories( array(
+            'hide_empty' => 1,
+        ) );
+ 
+        // Count the number of categories that are attached to the posts
+        $all_the_cool_cats = count( $all_the_cool_cats );
+ 
+        set_transient( 'all_the_cool_cats', $all_the_cool_cats );
+    }
+ 
+    if ( '1' != $all_the_cool_cats ) {
+        // This blog has more than 1 category so armored_categorized_blog should return true
+        return true;
+    } else {
+        // This blog has only 1 category so armored_categorized_blog should return false
+        return false;
+    }
+}
+ 
+/**
+ * Flush out the transients used in armored_categorized_blog
+ *
+ * @since armored 1.0
+ */
+function armored_category_transient_flusher() {
+    // Like, beat it. Dig?
+    delete_transient( 'all_the_cool_cats' );
+}
+add_action( 'edit_category', 'armored_category_transient_flusher' );
+add_action( 'save_post', 'armored_category_transient_flusher' );
+
+if ( ! function_exists( 'armored_comment' ) ) :
 /**
  * Template for comments and pingbacks.
- * 
+ *
  * Used as a callback by wp_list_comments() for displaying the comments.
  *
- * @since Armored 1.0
+ * @since armored 1.0
  */
 function armored_comment( $comment, $args, $depth ) {
     $GLOBALS['comment'] = $comment;
@@ -172,4 +172,4 @@ function armored_comment( $comment, $args, $depth ) {
             break;
     endswitch;
 }
-endif; // ends check for armored_comment() 
+endif; // ends check for armored_comment()
